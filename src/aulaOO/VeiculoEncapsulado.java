842e07aa26a19b8fa0e84188591074c6695ea1e2
36118482018.2@senacrjedu.local <36118482018.2@senacrjedu.local>
2019-04-13 10:45:44 -0300

    Commit Projeto Inteiro

---
+++ src/aulaOO/VeiculoEncapsulado.java
@@ -0,0 +1,15 @@
+package aulaOO;
+
+//Encapsulamento
+public class VeiculoEncapsulado {
+	public static void main(String[] args) {
+		Veiculo v1;
+		v1 = new Veiculo();
+		v1.nome = "Gol";
+		System.out.println(v1.nome);
+		for (int i = 1; i <= 5; i++) {
+			v1.acelera();
+		}
+	}
+}
+
